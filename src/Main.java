bd3e878e85e6215d7e552f34eb7c48c8c0d3fd7b
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-24 15:01:02 +0300

    HomeWork 1.12 забытый сеттер

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         Book book = new Book("Конструкторы в Java", author, 2023);
         Author author2 = new Author("Конструктор", "Явовный");
         Book book2 = new Book("Java в конструкторах", author2, 2023);
+        book2.setBookYear(2023);
     }
 }
