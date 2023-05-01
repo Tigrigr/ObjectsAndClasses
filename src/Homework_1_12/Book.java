fa3cee91ed41d99597fb487d22dd256e1d1cbc96
mikha <Mikhailov.dzenphone@gmail.com>
2023-05-01 11:14:28 +0300

    HomeWork 1.13, hashcode, equals, toString

--- src/Homework_1_12/Book.java
+++ src/Homework_1_12/Book.java
@@ -1,5 +1,7 @@
 package Homework_1_12;
 
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -26,6 +28,28 @@ public class Book {
 
     public void setBookYear(int bookYear) {
         this.bookYear = bookYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return bookYear == book.bookYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, bookYear);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", bookYear=" + bookYear +
+                '}';
     }
 }
 
